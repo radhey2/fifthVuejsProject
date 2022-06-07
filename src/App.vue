79cc7c2c5a22b3c21501a87d0d36c10f4a752e72
lenovo <radheshyamb9@gmail.com>
2022-06-07 23:41:06 +0530

    ADDED :  use mustache templates to dynamically update

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+ <div id="app">
+  <h3>What is your favorite kind of taco?</h3>
+  <p v-text="tacos"></p>
+  <p>{{ tacos }}</p>
+  <p><input v-model="tacos" /></p>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+    data() {
+    return {
+      tacos: 'I like Al Pastor tacos'
+    }
+  }
+}
+</script>
